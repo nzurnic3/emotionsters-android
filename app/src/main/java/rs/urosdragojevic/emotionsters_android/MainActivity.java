2c5035a3ace184b445c620b04412ad1ef4acac05
nzurnic3 <nikola.zurnic@gmail.com>
2023-06-23 17:11:49 +0200

    homescreen UI completed

--- app/src/main/java/rs/urosdragojevic/emotionsters_android/MainActivity.java
+++ app/src/main/java/rs/urosdragojevic/emotionsters_android/MainActivity.java
@@ -1,10 +1,18 @@
 package rs.urosdragojevic.emotionsters_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -15,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private TextView textView1;
+    private Toolbar toolbar;
+
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.bubble_text);
         textView1 = findViewById(R.id.bubble_text1);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.1.89:8080/emotionsters/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -53,5 +66,48 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+
+//        MenuItem.OnActionExpandListener onActionExpandListener = new MenuItem.OnActionExpandListener() {
+//            @Override
+//            public boolean onMenuItemActionExpand(@NonNull MenuItem item) {
+//                Toast.makeText(MainActivity.this, "search is expanded", Toast.LENGTH_SHORT).show();
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onMenuItemActionCollapse(@NonNull MenuItem item) {
+//                Toast.makeText(MainActivity.this, "search is collapsed", Toast.LENGTH_SHORT).show();
+//                return false;
+//            }
+//        };
+//        menu.findItem(R.id.search).setOnActionExpandListener(onActionExpandListener);
+        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("Search data here...");
+
+        return true;
+    }
+//    protected void onSaveInstanceState(Bundle outState) { NESTO ZA UKLANJANJE TEKSTA NAKON SEARCHA
+//        super.onSaveInstanceState(outState);
+//        searchView.setQuery("", false);
+//        searchView.setIconified(true);
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.account) {
+            Toast.makeText(this, "account is pressed", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.search) {
+            Toast.makeText(this, "search is pressed", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.menu) {
+            Toast.makeText(this, "menu is pressed", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
